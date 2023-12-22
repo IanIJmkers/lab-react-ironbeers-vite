18fec2a1dd61668aeafa2f92c542b2337e59c74f
Ian IJmkers <ianymkers@gmail.com>
2023-12-22 23:43:58 +0100

    Iteration 7 somewhat began

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddBeerPage() {
+  const [name, setName] = useState('')
+  const [tagline, setTagline] = useState('')
+  const [description, setDescription] = useState('')
+  const [first_brewed, setFirstBrewed] = useState('')
+  const [brewers_tips, setBrewersTips] = useState('')
+  const [attenuation_level, setAttenuationLevel] = useState('')
+  const [contributed_by, setContributedBy] = useState('')
+
   return (
     <div>
-      
+      <h1 className='text-2xl'> Create your own beer</h1>
+      <form action="">
+        <div>
+          <label htmlFor="">Name:</label>
+          <input type="text" value=''/>
+        </div>
+
+        <div>
+          <label htmlFor="">Tagline:</label>
+          <input type="text" value=''/>
+        </div>
+
+        <div>
+          <label htmlFor="">Description:</label>
+          <input type="text" value=''/>
+        </div>
+
+        <div>
+          <label htmlFor="">First brewed:</label>
+          <input type="text" value=''/>
+        </div>
+
+        <div>
+          <label htmlFor="">Brewers tips:</label>
+          <input type="text" value=''/>
+        </div>
+
+        <div>
+          <label htmlFor="">Attenuation:</label>
+          <input type="numbervalue=''" />
+        </div>
+
+        <div>
+          <label htmlFor="">Contributed by:</label>
+          <input type="text" value=''/>
+        </div>
+
+        <button type="submit">Create!</button>
+      </form>
     </div>
   )
 }
